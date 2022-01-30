4beb7d601bd463269c6e0b3ff3a4137897b096ae
gonzaloeheredia <gonzaloemanuelheredia@gmail.com>
2022-01-29 21:02:36 -0300

    14th commit

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail'
 import Products from './ProductsList/Products';
 
@@ -7,11 +8,16 @@ import Products from './ProductsList/Products';
 const ItemDetailContainer = () => {
 
   const  [itemsDetails, getItems]= useState([])
+  const  {id} = useParams()
 
   useEffect(()=>{
         new Promise ((resolved, rejected) => {
           setTimeout( () => { 
-              resolved(Products)
+            if (category) {
+              resolved(Products.find((p) => p.category === Products.id));
+            } else {
+              resolved(Products);
+            }
           },  1000)
         }
         ).then(
